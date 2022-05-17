e704c4a7c7f8c833f70d2c7e9d92e7d88688b083
Harena <53835196+HarenaTiavina@users.noreply.github.com>
2022-05-17 18:10:26 +0300

    ajout choix compte et création de compte

---
+++ SmartKid/app/src/main/java/com/smartkid/SpinnerActivity.java
@@ -0,0 +1,49 @@
+package com.smartkid;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class SpinnerActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_register);
+
+        //Récupération du Spinner déclaré dans le fichier main.xml de res/layout
+        spinner = (Spinner) findViewById(R.id.spinner);
+        //Création d'une liste d'élément à mettre dans le Spinner(pour l'exemple)
+        List exempleList = new ArrayList();
+        exempleList.add("Assinie");
+        exempleList.add("Bassam");
+        exempleList.add("Abidjan");
+
+        /*Le Spinner a besoin d'un adapter pour sa presentation alors on lui passe le context(this) et
+                un fichier de presentation par défaut( android.R.layout.simple_spinner_item)
+        Avec la liste des elements (exemple) */
+        ArrayAdapter adapter = new ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                Collections.singletonList(exempleList));
+
+
+        /* On definit une présentation du spinner quand il est déroulé         (android.R.layout.simple_spinner_dropdown_item) */
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Enfin on passe l'adapter au Spinner et c'est tout
+        spinner.setAdapter(adapter);
+
+    }
+}
